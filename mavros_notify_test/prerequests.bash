7417010296006a395631dc5d32e10d5cd2921dd1
Andrey Stepanov <astepanov@ntrlab.ru>
2021-12-08 21:08:39 +0700

    remove ardupilot_gazebo from dependencies

--- mavros_notify_test/prerequests.bash
+++ mavros_notify_test/prerequests.bash
@@ -8,12 +8,3 @@ source ~/.profile
 ./waf configure --board sitl
 ./waf copter
 cd ..
-git clone https://github.com/khancyr/ardupilot_gazebo
-cd ardupilot_gazebo
-git checkout 9bae0ead65f3a20f6a1c06f3887f92172abf79a9
-mkdir build
-cd build
-cmake ..
-make -j
-sudo make install
-cd ..
